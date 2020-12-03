84698b73a966285b1b784c2b3a09ceb8ae1b830b
Ninetta11 <ninawelsh1@outlook.com>
2020-12-03 14:19:14 +1100

    Updated portfolio with latest project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 
 var directoryEl = document.getElementById("directory");
 
-
+// changes nav-links to active on click
 directoryEl.addEventListener('click', function (event) {
     $(directoryEl.children).removeClass("active");
     $(event.target).addClass("active");
-})
+});
